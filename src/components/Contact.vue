a22b585ba0d128de657b2917a42fb0b005a50ab5
Muhammad Arslan Aslam <marslan@nisum.com>
2021-08-25 21:07:54 +0500

    create contacts list UI

---
+++ src/components/Contact.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="contact-container">
+    <div class="contact-row">
+      <div class="contact-column-value name">
+        {{ contact.name }}
+      </div>
+    </div>
+    <div class="contact-row">
+      <div class="contact-column-property">
+        Email:
+      </div>
+      <div class="contact-column-value">
+        someone@example.com
+      </div>
+    </div>
+    <div class="contact-row">
+      <div class="contact-column-property">
+        Phone:
+      </div>
+      <div class="contact-column-value">
+        +92-313-0000123
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Contact',
+    props: {
+      contact: Object
+    }
+  }
+</script>
+
+<style>
+  .contact-container {
+    display: flex;
+    flex-direction: column;
+    width: 31%;
+    padding: 20px 10px;
+    margin: 1px;
+    border: 1px transparent;
+    border-radius: 4px;
+    cursor: default;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .contact-container:hover {
+    background-color: #d7fbeb;
+  }
+
+  .contact-row {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .contact-column-property {
+    display: flex;
+    width: 20%;
+  }
+
+  .contact-column-value {
+    font-weight: bold;
+  }
+
+  .contact-column-value.name {
+    font-size: 18px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="contacts-list">
+      <h1>
+        Address Book
+        <router-link to='/' class="add-contact-btn">+ Add New Contact</router-link>
+      </h1>
+      <div class="contacts-container">
+        <Contact v-for="contact in contacts" :key="contact.email" :contact="contact" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Contact from '@/components/Contact.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Contact
+  },
+  data: function() {
+    return {
+      contacts: [
+        {
+          name: 'Iron Man',
+          email: 'iron.man@avengers.inc',
+          phone: '+1-555-555-5555'
+        },
+        {
+          name: 'Black Widow',
+          email: 'black.widow@avengers.inc',
+          phone: '+1-555-555-5555'
+        },
+        {
+          name: 'Captain America',
+          email: 'cap@avengers.inc',
+          phone: '+1-555-555-5555'
+        },
+        {
+          name: 'Spider Man',
+          email: 'coming.home@avengers.inc',
+          phone: '+1-555-555-5555'
+        },
+        {
+          name: 'Winter Soldier',
+          email: 'winter.soldier@avengers.inc',
+          phone: '+1-555-555-5555'
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style scoped>
+  .contacts-list {
+    max-width: 980px;
+    margin: 0 auto;
+    display: flex;
+    height: auto;
+    flex-direction: column;
+  }
+
+  .add-contact-btn {
+    background: #42b983;
+    padding: 10px 15px;
+    text-align: center;
+    font-weight: bold;
+    border: 1px transparent;
+    border-radius: 4px;
+    outline: none;
+    color: white;
+    text-decoration: none;
+    font-size: 14px;
+  }
+
+  .contacts-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    margin-top: 50px;
+    flex-wrap: wrap;
+  }
+</style>
